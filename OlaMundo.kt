1aaefb259b1f09504ce8466414b5292513886258
Danilo Mendonça <danilomendonca08@gmail.com>
2024-02-22 20:11:15 -0300

    arquivos do kotlin

---
+++ OlaMundo.kt
@@ -0,0 +1,16 @@
+//PARA FAZER COMENTÁRIO DE LINHA
+
+/*
+
+PARA FAZER UM COMENTÁRIO DE BLOCO
+
+ */
+
+ //Aplicativo quando compilado
+ // .apk - no Android
+ // .ipa - no iOS
+
+//Aqui começamos definindo uma função
+fun main () {
+    println("Olá, Mundo!")
+}
