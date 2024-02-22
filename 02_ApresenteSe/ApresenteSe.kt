399a4be181a5e72b2f1df9e8c8a9fc7ffb263373
Danilo Mendonça <danilomendonca08@gmail.com>
2024-02-22 20:50:44 -0300

    update

---
+++ 02_ApresenteSe/ApresenteSe.kt
@@ -0,0 +1,15 @@
+//Declarando varáveis em Kotlin
+
+fun main() {
+    
+    //Declaração das variáveis
+    var idade = 19 
+    var altura = 1.68 
+    var inicial = 'D'
+
+    //Imprimir as informações - interpolação de strings
+    println("A inicial do meu nome é ${inicial}")
+    println("A minha idade é ${idade} e minha altura é ${altura} metros de altura") 
+
+
+}
